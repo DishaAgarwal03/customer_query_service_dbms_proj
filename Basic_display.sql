445b289061def7029314f7ede2ddbe9587ca15e2
DishaAgarwal03 <97750861+DishaAgarwal03@users.noreply.github.com>
2023-05-11 16:45:34 +0530

    Create Basic_display.sql

---
+++ Basic_display.sql
@@ -0,0 +1,15 @@
+--display the structures 
+desc credentials;
+desc customer;
+desc agent;
+desc conversation;
+desc message;
+
+--display the content 
+select * from credentials;
+select * from customer;
+select * from agent;
+select * from conversation;
+select * from message;
+
+
